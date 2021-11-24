a205e6ad4a7ab26cc1a67523ef8da8b428eff6df
tplamondon <tplamondon@tutanota.de>
2021-11-24 13:21:00 -0800

    put in logic.js

---
+++ src/logic.js
@@ -0,0 +1,30 @@
+
+
+
+//based on HP and AC
+/*
+Read down the Hit Points column of the Monster Statistics by Challenge Rating table until you find your monster's hit points. 
+Then look across and note the challenge rating suggested for a monster with those hit points.
+
+Now look at the Armor Class suggested for a monster of that challenge rating. If your monster's AC is at least two points higher 
+or lower than that number, adjust the challenge rating suggested by its hit points up or down by 1 for every 2 points of difference.
+*/
+function calcDefensiveCR(){
+
+}
+
+
+/*
+Read down the Damage/Round column of the Monster Statistics by Challenge Rating table until you find your monster's damage output per round. 
+Then look across and note the challenge rating suggested for a monster that deals that much damage.
+
+Now look at the attack bonus suggested for a monster of that challenge rating. If your monster's attack bonus is at least two points higher 
+or lower than that number, adjust the challenge rating suggested by its damage output up or down by 1 for every 2 points of difference.
+
+If the monster relies more on effects with saving throws than on attacks, use the monster's save DC instead of its attack bonus.
+
+If your monster uses different attack bonuses or save DCs, use the ones that will come up the most often.
+*/
+function calcOffensiveCR(){
+    
+}
